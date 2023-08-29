2f3132fac6e1001bbf37c3dde6368d31303ab35a
linoverlin <verlin@lumonta.com>
2023-08-29 14:32:52 +0700

    kk baru

--- template-surat-premium/surat_permohonan_perubahan_kartu_keluarga_baru/f-1.02.php
+++ template-surat-premium/surat_permohonan_perubahan_kartu_keluarga_baru/f-1.02.php
@@ -2,6 +2,6 @@
 
     defined('BASEPATH') || exit('No direct script access allowed');
 
-    include(FCPATH . "/template-surat/lampiran/f-1.01/view.php");
+    include(FCPATH . "/template-surat/lampiran/f-1.02/view.php");
 
 ?>
